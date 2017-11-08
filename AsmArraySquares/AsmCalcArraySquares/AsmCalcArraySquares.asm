9273061d396b2461147d69972106519828b75d9c
GitLeeRepo <traeven99.gmail.com>
2017-11-08 04:55:41 -0700

    Converted tabs to spaces

--- AsmArraySquares/AsmCalcArraySquares/AsmCalcArraySquares.asm
+++ AsmArraySquares/AsmCalcArraySquares/AsmCalcArraySquares.asm
@@ -14,42 +14,42 @@
 
 CalcArraySquares_  proc
 
-		; Function prolog
+        ; Function prolog
         push ebp
         mov ebp,esp
-		push ebx
-		push esi
-		push edi
+        push ebx
+        push esi
+        push edi
 
-		; Load Arguments
-		mov esi,[ebp+8]							; srcArray
-		mov edi,[ebp+12]						; destArray
-		mov ecx,[ebp+16]						; numElements
-												; srcMultFactor [ebp+20] used below
+        ; Load Arguments
+        mov esi,[ebp+8]                         ; srcArray
+        mov edi,[ebp+12]                        ; destArray
+        mov ecx,[ebp+16]                        ; numElements
+                                                ; srcMultFactor [ebp+20] used below
 
-		; Initialization
-		xor eax,eax								; zero to track running sum
-		cmp ecx,0                               ; make sure NumElements != 0
-		jle EmptyArray
-		shl ecx,2                               ; size of array (numElements * dword size)
-		xor ebx,ebx								; zero to index first element
+        ; Initialization
+        xor eax,eax                             ; zero to track running sum
+        cmp ecx,0                               ; make sure NumElements != 0
+        jle EmptyArray
+        shl ecx,2                               ; size of array (numElements * dword size)
+        xor ebx,ebx                             ; zero to index first element
 
-		; Repeat until finished
-@@:		mov edx,[esi+ebx]						; get srcArray element value
-		imul edx,[ebp+20]						; srcMultFactor
-		imul edx,edx							; square the srcArray element
-		mov [edi+ebx],edx						; save result to destArray[esi+ebx]
-		add eax,edx                             ; running sum
-		add ebx,4                               ; increment array element offset
-		cmp ebx,ecx
-		jl @B									; repeat until end of array reached
+        ; Repeat until finished
+@@:     mov edx,[esi+ebx]                       ; get srcArray element value
+        imul edx,[ebp+20]                       ; srcMultFactor
+        imul edx,edx                            ; square the srcArray element
+        mov [edi+ebx],edx                       ; save result to destArray[esi+ebx]
+        add eax,edx                             ; running sum
+        add ebx,4                               ; increment array element offset
+        cmp ebx,ecx
+        jl @B                                   ; repeat until end of array reached
 
 EmptyArray:
-		; Function epilog
-		pop edi
-		pop esi
-		pop ebx
+        ; Function epilog
+        pop edi
+        pop esi
+        pop ebx
         pop ebp
-        ret										; eax = sum
+        ret	                                    ; eax = sum
 CalcArraySquares_  endp
         end
